8f6b6c4f6db16e9cde4a5ea8a7f885ddbf5aaaed
mykelgozie <mykelgozie@gmail.com>
2021-05-29 22:40:38 +0100

    fix path

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-app-heroku'));
+app.use(express.static(__dirname + '/dist/seampage'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angular-app-heroku/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/seampage/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
